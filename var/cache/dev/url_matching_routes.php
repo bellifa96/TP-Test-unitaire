2067f402e4c15b3fb26417cad0c5a205b96a0ac7
bellifa96 <bellifa96@live.fr>
2020-07-15 14:08:51 +0200

    calculatrice simple et basique

--- var/cache/dev/url_matching_routes.php
+++ var/cache/dev/url_matching_routes.php
@@ -13,16 +13,7 @@ return [
         '/_profiler/search_bar' => [[['_route' => '_profiler_search_bar', '_controller' => 'web_profiler.controller.profiler::searchBarAction'], null, null, null, false, false, null]],
         '/_profiler/phpinfo' => [[['_route' => '_profiler_phpinfo', '_controller' => 'web_profiler.controller.profiler::phpinfoAction'], null, null, null, false, false, null]],
         '/_profiler/open' => [[['_route' => '_profiler_open_file', '_controller' => 'web_profiler.controller.profiler::openAction'], null, null, null, false, false, null]],
-        '/editor' => [[['_route' => 'editor', '_controller' => 'App\\Controller\\EditorController::index'], null, null, null, false, false, null]],
-        '/create_editor' => [[['_route' => 'create_editor', '_controller' => 'App\\Controller\\EditorController::newAction'], null, null, null, false, false, null]],
-        '/' => [[['_route' => 'game', '_controller' => 'App\\Controller\\JeuxController::index'], null, null, null, false, false, null]],
-        '/create_game' => [[['_route' => 'create_game', '_controller' => 'App\\Controller\\JeuxController::newAction'], null, null, null, false, false, null]],
-        '/login' => [[['_route' => 'app_login', '_controller' => 'App\\Controller\\SecurityController::login'], null, null, null, false, false, null]],
-        '/logout' => [[['_route' => 'app_logout', '_controller' => 'App\\Controller\\SecurityController::logout'], null, null, null, false, false, null]],
-        '/update_user' => [[['_route' => 'update_user', '_controller' => 'App\\Controller\\UpdateUserController::newAction'], null, null, null, false, false, null]],
-        '/profile' => [[['_route' => 'profile', '_controller' => 'App\\Controller\\UserController::index'], null, null, null, false, false, null]],
-        '/user_list' => [[['_route' => 'user', '_controller' => 'App\\Controller\\UserController::usersList'], null, null, null, false, false, null]],
-        '/create_user' => [[['_route' => 'form', '_controller' => 'App\\Controller\\UserController::newAction'], null, null, null, false, false, null]],
+        '/calculator' => [[['_route' => 'calculator', '_controller' => 'App\\Controller\\CalculatorController::calculate'], null, null, null, false, false, null]],
     ],
     [ // $regexpList
         0 => '{^(?'
@@ -41,18 +32,6 @@ return [
                         .'|(*:159)'
                     .')'
                 .')'
-                .'|/editor_details/([^/]++)(*:193)'
-                .'|/delete_(?'
-                    .'|editor/([^/]++)(*:227)'
-                    .'|game/([^/]++)(*:248)'
-                    .'|user/([^/]++)(*:269)'
-                .')'
-                .'|/game_details/([^/]++)(*:300)'
-                .'|/update_(?'
-                    .'|editor/([^/]++)(*:334)'
-                    .'|game/([^/]++)(*:355)'
-                .')'
-                .'|/admin_update_user/([^/]++)(*:391)'
             .')/?$}sDu',
     ],
     [ // $dynamicRoutes
@@ -62,16 +41,8 @@ return [
         116 => [[['_route' => '_profiler_router', '_controller' => 'web_profiler.controller.router::panelAction'], ['token'], null, null, false, false, null]],
         136 => [[['_route' => '_profiler_exception', '_controller' => 'web_profiler.controller.exception_panel::body'], ['token'], null, null, false, false, null]],
         149 => [[['_route' => '_profiler_exception_css', '_controller' => 'web_profiler.controller.exception_panel::stylesheet'], ['token'], null, null, false, false, null]],
-        159 => [[['_route' => '_profiler', '_controller' => 'web_profiler.controller.profiler::panelAction'], ['token'], null, null, false, true, null]],
-        193 => [[['_route' => 'editor_details', '_controller' => 'App\\Controller\\EditorController::editorDetail'], ['id'], null, null, false, true, null]],
-        227 => [[['_route' => 'editor_delete', '_controller' => 'App\\Controller\\EditorController::delete'], ['id'], null, null, false, true, null]],
-        248 => [[['_route' => 'game_delete', '_controller' => 'App\\Controller\\JeuxController::delete'], ['id'], null, null, false, true, null]],
-        269 => [[['_route' => 'user_delete', '_controller' => 'App\\Controller\\UserController::delete'], ['id'], null, null, false, true, null]],
-        300 => [[['_route' => 'game_details', '_controller' => 'App\\Controller\\JeuxController::editorDetail'], ['id'], null, null, false, true, null]],
-        334 => [[['_route' => 'update_editor', '_controller' => 'App\\Controller\\UpdateEditorController::Action'], ['id'], null, null, false, true, null]],
-        355 => [[['_route' => 'update_game', '_controller' => 'App\\Controller\\UpdateJeuxController::Action'], ['id'], null, null, false, true, null]],
-        391 => [
-            [['_route' => 'admin_update_user', '_controller' => 'App\\Controller\\UpdateUserController::Action'], ['id'], null, null, false, true, null],
+        159 => [
+            [['_route' => '_profiler', '_controller' => 'web_profiler.controller.profiler::panelAction'], ['token'], null, null, false, true, null],
             [null, null, null, null, false, false, 0],
         ],
     ],
