5b8534714e0fb6d059fa9354ad3ff40f8590a7f0
bellifa96 <bellifa96@live.fr>
2020-07-16 14:53:54 +0200

    isValid()

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -18,8 +18,31 @@ class UserTest extends TestCase
      $user->setEmail('osm@live.fr');
      $this->assertNotEmpty($user->getFirstName());
      $this->assertNotEmpty($user->getLastName());
+     $this->assertEquals('oussama',$user->getFirstName());
+     $this->assertEquals('bellifa',$user->getLastName());
+     $this->assertEquals('13',$user->getAge());
+     $this->assertEquals('osm@live.fr',$user->getEmail());
      $this->assertGreaterThanOrEqual('13',$user->getAge());
-     $this->assertNotEmpty($user->getEmail());
+     $this->assertRegExp('/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/',$user->getEmail());
+     $this->assertEquals(true,$this->isValid($user));
+   }
 
+   public function isValid(User $user){
+     if( empty($user->getFirstName()) || count_chars($user->getFirstName()) < 3 ){
+       return "firstName is not valid";
+     }
+     if( empty($user->getLastName()) || count_chars($user->getLastName()) < 3 ){
+       return "lastName is not valid ";
+     }
+     if( empty($user->getAge()) || $user->getAge() < 13 ){
+       return "age is not valid ";
+     }
+     $expression = "/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/"  ;
+     if( empty($user->getEmail()) || !filter_var($user->getEmail(),FILTER_VALIDATE_EMAIL) ){
+       {
+         return "email is not valid";
+       }
+     }
+     return true;
    }
 }
